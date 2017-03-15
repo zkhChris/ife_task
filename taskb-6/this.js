cfe3b12bb5907a1ff6f4099c6ef4470b14b16e2f
zhangkehan <373877021@qq.com>
2017-03-15 21:49:17 +0800

    taskb-6

--- taskb-6/this.js
+++ taskb-6/this.js
@@ -1,30 +1,27 @@
 (function(){
-  var inputB=document.getElementById("in");
+  var contentB=document.getElementById("in");
   var content=document.getElementById("info");
   var q=document.getElementById("voc");
   var qSubmit=document.getElementById("vocSub");
   var list=document.getElementById("list");
 
-  inputB.onclick=function(){
+  contentB.onclick=function(){
     if(content.value===""||content.value===null){
       alert("请输入");
     }else{
-    if(content.value.indexOf(",")<0){
-      var span=document.createElement("span");
-      var text=document.createTextNode(content.value);
-      span.appendChild(text);
-      list.appendChild(span);
-    }else{
-      var arr=content.value.split(",");
+      var arr=content.value.split(/[,，、\r\n\t\v\s]/g);
       for(var i=0;i<arr.length;i++){
         var span=document.createElement("span");
         var text=document.createTextNode(arr[i]);
         span.appendChild(text);
-        list.appendChild(span);
+        list.appendChild(span).onclick=function(){
+          alert(this.innerText);
+          list.removeChild(this);
+        };
       }
     }
   }
-  };
+
 
     qSubmit.onclick=function(){
       var text=q.value;
@@ -38,7 +35,60 @@
           items[i].style.backgroundColor="white";
         }
       }
-
     }
+
+    content.onchange=function(){
+      return content.value;
+    };
+    document.getElementById("rightIn").onclick=function(){
+      var v=content.onchange();
+      if (v!==null&&v!==""){
+      var node=document.createElement("span");
+      var text=document.createTextNode(v);
+      node.appendChild(text);
+      list.appendChild(node).onclick=function(){//新增元素并添加事件
+        alert(this.innerText);
+        list.removeChild(this);
+      };
+      }
+    }
+    document.getElementById("leftIn").onclick=function(){
+      var v=content.onchange();
+      if (v!==null&&v!==""){
+      var node=document.createElement("span");
+      var text=document.createTextNode(v);
+      node.appendChild(text);
+      //判断list内有无元素
+      var span=document.getElementById("list").getElementsByTagName("span");
+      var hasElement=span.length;
+      if(hasElement>0){
+        list.insertBefore(node,span[0]).onclick=function(){//新增元素并添加事件
+            alert(this.innerText);
+            list.removeChild(this);
+          };
+      }else{
+        list.appendChild(node).onclick=function(){
+          alert(this.innerText);
+          list.removeChild(this);
+        };
+      }
+    }
+    };
+    document.getElementById("leftOut").onclick=function(){
+      var span=document.getElementById("list").getElementsByTagName("span");
+      var leng=span.length;
+      if(leng>0){
+        alert(span[0].innerText);
+        list.removeChild(span[0]);
+      }
+    };
+    document.getElementById("rightOut").onclick=function(){
+      var span=document.getElementById("list").getElementsByTagName("span");
+      var leng=span.length;
+      if(leng>0){
+        alert(span[leng-1].innerText);
+        list.removeChild(span[leng-1]);
+      }
+    };
 
 })()
